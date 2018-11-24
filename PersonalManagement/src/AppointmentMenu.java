d93afe5d0ae3ca8e308985f714e33952efa79f33
jjisol <jjisol97@gmail.com>
2018-11-25 00:33:37 +0900

    coding create, update, delete method by jjisol

--- PersonalManagement/src/AppointmentMenu.java
+++ PersonalManagement/src/AppointmentMenu.java
@@ -1,6 +1,12 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Scanner;
+import java.util.Set;
 
 public class AppointmentMenu {
+	int appointment_id = 0;
+	public static HashMap<Integer, Appointment> inputAppointment;
 	public void PrintMenu() {
 	      System.out.println("---------------------------");
 	      System.out.println("1. Create Appointment");
@@ -23,7 +29,6 @@ public class AppointmentMenu {
 		      if(receive_num==-1) {
 		    	  	repeat=false;
 		      }
-		      scanner.close();
 		   }while(repeat);
 	      
 	   }
@@ -48,17 +53,82 @@ public class AppointmentMenu {
 		   
 		   return 0;
 	   }
+	   
 	   public void createAppointment() {
-	      
+		   Scanner scanner = new Scanner(System.in);
+		   inputAppointment = new HashMap<Integer, Appointment>();
+		   boolean repeat = true;
+		   do {
+			   System.out.println("일정을 입력해주세요.");
+			   System.out.print("날짜(년/월/일): ");
+			   String date = scanner.nextLine();
+			   System.out.print("사람: ");
+			   String person = scanner.nextLine();
+			   System.out.print("장소: ");
+			   String location = scanner.nextLine();
+			   inputAppointment.put(++appointment_id, new Appointment(date, person, location));
+			   
+			   System.out.print("추가 입력 하시겠습니까?(y/n): ");
+			   String answer=scanner.nextLine();
+			   if(answer.equals("n")) {
+				   repeat=false;
+			   }
+		   }while(repeat);
 	   }
 	   
 	   public void viewAppointment() {
+		   System.out.println("==================================================");
+		   System.out.println("id 날짜 \t\t 사람 \t 장소\t");
+		   System.out.println("==================================================");
+		   
+		   Set<Integer>keys = inputAppointment.keySet();
+		   Iterator<Integer>iterate = keys.iterator();
+		   while(iterate.hasNext()){
+			   Integer id = iterate.next();
+			   Appointment appointment = inputAppointment.get(id);
+			   if(appointment!=null&&id!=null)
+				   System.out.println(id+" "+appointment.getDate()+"\t"+appointment.getPerson()+"\t"+appointment.getLocation());
+		   }
 	      
 	   }
+	   
 	   public void updateAppointment() {
-	      
+		   boolean repeat = true;
+		   do{
+			   Scanner scanner = new Scanner(System.in);
+			   System.out.print("수정할 id 입력: ");
+			   int id = scanner.nextInt();
+			   System.out.print("날짜(년/월/일): ");
+			   String date = scanner.next();
+			   System.out.print("사람: ");
+			   String person = scanner.next();
+			   System.out.print("장소: ");
+			   String location = scanner.next();
+			   inputAppointment.put(id, new Appointment(date, person, location));
+			   
+			   System.out.print("추가 수정하시겠습니까?(y/n)");
+			   String answer=scanner.next();
+			   if(answer.equals("n")) {
+				   repeat=false;
+			   }
+		   }while(repeat);
+		   
 	   }
+	   
 	   public void deleteAppointment() {
+		   boolean repeat = true;
+		   do {
+			   Scanner scanner = new Scanner(System.in);
+			   System.out.print("삭제할 id 입력: ");
+			   int id = scanner.nextInt();
+			   inputAppointment.remove(id);
+			   
+			   System.out.print("추가 삭제하시겠습니까?(y/n)");
+			   String answer=scanner.next();
+			   if(answer.equals("n")) {
+				   repeat=false;
+			   }
+		   }while(repeat);
 	      
 	   }
 }
