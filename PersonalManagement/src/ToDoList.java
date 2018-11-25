97687cc6bcb5512667adf2c97c984b3c890c70b5
Zeusl <jje5ngzsos@gmail.com>
2018-11-26 04:17:32 +0900

    ToDoList change commited by Zeusl

--- PersonalManagement/src/ToDoList.java
+++ PersonalManagement/src/ToDoList.java
@@ -13,5 +13,37 @@ public class ToDoList{
        this.description=description;
     }
 
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getCreateDate() {
+		return create_date;
+	}
+	
+	public void setCreateDate(String person) {
+		this.create_date = create_date;
+	}
+	
+	public String getDue() {
+		return due;
+	}
+	
+	public String setDue(String due) {
+		this.due=due;
+	}
+	
+	public void getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = descriptioin;
+	}
 	 
+    
 }
